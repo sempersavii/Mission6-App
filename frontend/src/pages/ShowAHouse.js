c140a52e4e78a8e0eae441b33516a2cb4e98f467
Mimars-Project <nzbloxicoot@gmail.com>
2022-03-24 12:45:44 +1300

    Added connections from frontend to backend database

--- frontend/src/pages/ShowAHouse.js
+++ frontend/src/pages/ShowAHouse.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import HousesDataService from "../services/house";
 
 import Bed from '../images/Bed.png';
 import Bathtub from '../images/Bathtub.png';
@@ -8,11 +9,114 @@ import Car from '../images/Car.png';
 
 import './ShowAHouse.css';
 
-const ShowAHouse = () => {
+const ShowAHouse = (props) => {
+  let params = useParams();
+  const initialHouseState = {
+    id: null,
+    title: "",
+    location: "",
+    price: 0,
+    description: ""
+  }
+  const [house, setHouse] = useState(initialHouseState);
   const [subImage, setSubImage] = useState(1)
+
+  const retrieveHouse = id => {
+    HousesDataService.get(id)
+      .then(response => {
+        setHouse(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }
+
+  useEffect(() => {
+    retrieveHouse(params.id);
+  }, [params.id]);
+
     return (
       <>
+      {house._id ?
         <div className="App">
+          <div id='content-box'>
+            <div className='horizontal'>
+              <img className='main-image' src={subImage === 1 ? "./images/apartment1.jpg" : "./images/apartment3.jpg"} alt="" />
+            </div>
+            <div className='horizontal sub-images'>
+              <div className='sub-image-container'>
+                <img className={subImage === 1 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(1)} src="./images/apartment1.jpg" alt="" />
+              </div>
+              <div className='sub-image-container'>
+                <img className={subImage === 2 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(2)} src="./images/apartment3.jpg" alt="" />
+              </div>
+              <div className='sub-image-container'>
+                <img className={subImage === 3 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(3)} src="./images/apartment1.jpg" alt="" />
+              </div>
+            </div>
+            <div className='horizontal text-center'>
+              <img src={Bed} alt="bed" className='select-img' />Photos <img src={Bed} alt="bed" className='select-img' />Videos <img src={Bed} alt="bed" className='select-img' />Map
+            </div>
+            <div className='horizontal'>
+              <div className='column'>
+                <button className='button'>Apply online</button>{' '}
+                <Link to='/BookAView'><button className='button-secondary'>Book a viewing</button></Link>
+                <div className='heading2'>{house.title}</div>
+                <div className='sub-heading'>{house.location}</div>
+                <div className='sub-heading'>Apartment</div>
+                <div className='heading2'>$600 per week</div>
+                <div className='options-text'><img src={Bed} alt="bed" className='option-img' /> Apartment</div>
+                <div className='options-text'>
+                  <img src={Bed} alt="bed" className='option-img' /> 2 Bedrooms
+                  <img src={Bathtub} alt="bathtub" className='option-img' /> 1 Bathroom
+                  <img src={Car} alt="car" className='option-img' /> 1 Carpark
+                  <img src={NoAnimals} alt="noanimals" className='option-img' /> Pet friendly
+                </div>
+                <div className='heading3'>Available</div>
+                <div className='options-text'>Available from Friday 18th March 2022</div>
+                <div className='paragraph'>{house.description}</div>
+                <div className='heading3'>Property details</div>
+                <div className='paragraph'>2 double bedrooms with good quality mattresses<br/>
+                  1 spacious bathroom<br/>
+                  Rent Includes cold water<br/>
+                  Power, phone & internet not included<br/>
+                </div>
+                <div className='paragraph'>Move-in cost: 4 weeks bond and 1-week rent.</div>
+              </div>
+
+              <div className='column'>
+                <div className='agent'>
+                  <div className='top-agent'>
+                  </div>
+                  <img className='agent-image' src="./images/apartment1.jpg" alt="bed" /> 
+                  <div className='agent-details'>
+                    <div className='sub-heading'>Ashley Zhang</div>
+                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> 021 136 6789</div>
+                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> ashlee.zhang@akproperty.net</div>
+                  </div>
+                </div>
+                <div className='share-listing'>
+                  <div className='paragraph'>Share this listing: {' '}
+                    <img src={Bed} alt="bed" className='social-img' />{' '}
+                    <img src={Bed} alt="bed" className='social-img' />{' '}
+                    <img src={Bed} alt="bed" className='social-img' />{' '}
+                    <img src={Bed} alt="bed" className='social-img' />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div className='horizontal'>
+              <button className='button'>Apply online</button>{' '}
+              <Link to='/BookAView'><button className='button-secondary'>Book a viewing</button></Link>
+              <button className='return-search' href="/SearchAHouse">Return to Search <img className='return-img' src={Bed} alt="bed" /></button>
+            </div>
+          </div>
+        </div>
+      : 
+      <div className="App">
           <div id='content-box'>
             <div className='horizontal'>
               <img className='main-image' src={subImage === 1 ? "./images/apartment1.jpg" : "./images/apartment3.jpg"} alt="" />
@@ -88,6 +192,7 @@ const ShowAHouse = () => {
             </div>
           </div>
         </div>
+      }
       </>
     );
   }
