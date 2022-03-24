7e157d378a93ce42a18d357f56d9ea544f6925ff
Mimars-Project <nzbloxicoot@gmail.com>
2022-03-24 21:08:09 +1300

    Working on new page where id exists

--- frontend/src/pages/ShowAHouse.js
+++ frontend/src/pages/ShowAHouse.js
@@ -6,20 +6,27 @@ import Bed from '../images/Bed.png';
 import Bathtub from '../images/Bathtub.png';
 import NoAnimals from '../images/No Animals.png';
 import Car from '../images/Car.png';
+import Image from "../images/Image.png";
+import Video from "../images/Cinema.png";
+import Map from "../images/Map Marker.png";
+
+import ApartmentIcon from "../images/House.png";
+
+import Agent from "../images/Ashlee-square-2-150x150 1.png"
+import Phone from "../images/Phone.png";
+import Mail from "../images/Mail.png";
+import Print from "../images/Print.png";
+import FacebookLogo from "../images/Facebook F.png";
+import TwitterLogo from "../images/Twitter.png"
+import BackArrow from "../images/Arrow_Undo_Up_Left.png"
 
 import './ShowAHouse.css';
 
 const ShowAHouse = (props) => {
   let params = useParams();
-  const initialHouseState = {
-    id: null,
-    title: "",
-    location: "",
-    price: 0,
-    description: ""
-  }
-  const [house, setHouse] = useState(initialHouseState);
-  const [subImage, setSubImage] = useState(1)
+  const [house, setHouse] = useState({});
+  const [subImage, setSubImage] = useState(1);
+  const [subType, setSubType] = useState("Images")
 
   const retrieveHouse = id => {
     HousesDataService.get(id)
@@ -42,21 +49,21 @@ const ShowAHouse = (props) => {
         <div className="App">
           <div id='content-box'>
             <div className='horizontal'>
-              <img className='main-image' src={subImage === 1 ? "./images/apartment1.jpg" : "./images/apartment3.jpg"} alt="" />
+              <img className='main-image' src={subImage === 1 ? house.image : "./images/apartment3.jpg"} alt="" />
             </div>
             <div className='horizontal sub-images'>
               <div className='sub-image-container'>
-                <img className={subImage === 1 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(1)} src="./images/apartment1.jpg" alt="" />
+                <img className={subImage === 1 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(1)} src={house.image} alt="" />
               </div>
               <div className='sub-image-container'>
                 <img className={subImage === 2 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(2)} src="./images/apartment3.jpg" alt="" />
               </div>
               <div className='sub-image-container'>
-                <img className={subImage === 3 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(3)} src="./images/apartment1.jpg" alt="" />
+                <img className={subImage === 3 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(3)} src="./images/apartment3.jpg" alt="" />
               </div>
             </div>
             <div className='horizontal text-center'>
-              <img src={Bed} alt="bed" className='select-img' />Photos <img src={Bed} alt="bed" className='select-img' />Videos <img src={Bed} alt="bed" className='select-img' />Map
+              <img src={Image} alt="bed" className='select-img' />Photos <img src={Video} alt="bed" className='select-img' />Videos <img src={Map} alt="bed" className='select-img' />Map
             </div>
             <div className='horizontal'>
               <div className='column'>
@@ -65,12 +72,12 @@ const ShowAHouse = (props) => {
                 <div className='heading2'>{house.title}</div>
                 <div className='sub-heading'>{house.location}</div>
                 <div className='sub-heading'>Apartment</div>
-                <div className='heading2'>$600 per week</div>
-                <div className='options-text'><img src={Bed} alt="bed" className='option-img' /> Apartment</div>
+                <div className='heading2'>${house.price} per week</div>
+                <div className='options-text'><img src={ApartmentIcon} alt="House" className='option-img' /> Apartment</div>
                 <div className='options-text'>
                   <img src={Bed} alt="bed" className='option-img' /> 2 Bedrooms
                   <img src={Bathtub} alt="bathtub" className='option-img' /> 1 Bathroom
-                  <img src={Car} alt="car" className='option-img' /> 1 Carpark
+                  {house.carPark ? <img src={Car} alt="car" className='option-img' /> + house.carPark + "Carpark" : <></>}
                   <img src={NoAnimals} alt="noanimals" className='option-img' /> Pet friendly
                 </div>
                 <div className='heading3'>Available</div>
@@ -89,19 +96,19 @@ const ShowAHouse = (props) => {
                 <div className='agent'>
                   <div className='top-agent'>
                   </div>
-                  <img className='agent-image' src="./images/apartment1.jpg" alt="bed" /> 
+                  <img className='agent-image' src="./images/apartment1.jpg" alt="Agent" /> 
                   <div className='agent-details'>
                     <div className='sub-heading'>Ashley Zhang</div>
-                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> 021 136 6789</div>
-                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> ashlee.zhang@akproperty.net</div>
+                    <div className='agent-contact-text'><img src={Phone} alt="Phone" className='social-img' /> 021 136 6789</div>
+                    <div className='agent-contact-text'><img src={Mail} alt="Mail" className='social-img' /> ashlee.zhang@akproperty.net</div>
                   </div>
                 </div>
                 <div className='share-listing'>
                   <div className='paragraph'>Share this listing: {' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />
+                    <img src={Mail} alt="Mail" className='social-img' />{' '}
+                    <img src={Print} alt="Print" className='social-img' />{' '}
+                    <img src={FacebookLogo} alt="FacebookLogo" className='social-img' />{' '}
+                    <img src={TwitterLogo} alt="TwitterLogo" className='social-img' />
                   </div>
                 </div>
               </div>
@@ -111,7 +118,7 @@ const ShowAHouse = (props) => {
             <div className='horizontal'>
               <button className='button'>Apply online</button>{' '}
               <Link to='/BookAView'><button className='button-secondary'>Book a viewing</button></Link>
-              <button className='return-search' href="/SearchAHouse">Return to Search <img className='return-img' src={Bed} alt="bed" /></button>
+              <button className='return-search' href="/SearchAHouse">Return to Search <img className='return-img' src={BackArrow} alt="Back" /></button>
             </div>
           </div>
         </div>
@@ -129,11 +136,11 @@ const ShowAHouse = (props) => {
                 <img className={subImage === 2 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(2)} src="./images/apartment3.jpg" alt="" />
               </div>
               <div className='sub-image-container'>
-                <img className={subImage === 3 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(3)} src="./images/apartment1.jpg" alt="" />
+                <img className={subImage === 3 ? 'sub-image active' : 'sub-image'} onClick={() => setSubImage(3)} src="./images/apartment3.jpg" alt="" />
               </div>
             </div>
             <div className='horizontal text-center'>
-              <img src={Bed} alt="bed" className='select-img' />Photos <img src={Bed} alt="bed" className='select-img' />Videos <img src={Bed} alt="bed" className='select-img' />Map
+              <img src={Image} alt="bed" className='select-img' />Photos <img src={Video} alt="bed" className='select-img' />Videos <img src={Map} alt="bed" className='select-img' />Map
             </div>
             <div className='horizontal'>
               <div className='column'>
@@ -143,7 +150,7 @@ const ShowAHouse = (props) => {
                 <div className='sub-heading'>1106/207 Federal street , City Centre (Auckland City) , 1010 </div>
                 <div className='sub-heading'>Apartment</div>
                 <div className='heading2'>$600 per week</div>
-                <div className='options-text'><img src={Bed} alt="bed" className='option-img' /> Apartment</div>
+                <div className='options-text'><img src={ApartmentIcon} alt="House" className='option-img' /> Apartment</div>
                 <div className='options-text'>
                   <img src={Bed} alt="bed" className='option-img' /> 2 Bedrooms
                   <img src={Bathtub} alt="bathtub" className='option-img' /> 1 Bathroom
@@ -166,19 +173,19 @@ const ShowAHouse = (props) => {
                 <div className='agent'>
                   <div className='top-agent'>
                   </div>
-                  <img className='agent-image' src="./images/apartment1.jpg" alt="bed" /> 
+                  <img className='agent-image' src={Agent} alt="Agent" /> 
                   <div className='agent-details'>
                     <div className='sub-heading'>Ashley Zhang</div>
-                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> 021 136 6789</div>
-                    <div className='agent-contact-text'><img src={Bed} alt="bed" className='social-img' /> ashlee.zhang@akproperty.net</div>
+                    <div className='agent-contact-text'><img src={Phone} alt="Phone" className='social-img' /> 021 136 6789</div>
+                    <div className='agent-contact-text'><img src={Mail} alt="Mail" className='social-img' /> ashlee.zhang@akproperty.net</div>
                   </div>
                 </div>
                 <div className='share-listing'>
                   <div className='paragraph'>Share this listing: {' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />{' '}
-                    <img src={Bed} alt="bed" className='social-img' />
+                    <img src={Mail} alt="Mail" className='social-img' />{' '}
+                    <img src={Print} alt="Print" className='social-img' />{' '}
+                    <img src={FacebookLogo} alt="Facebook" className='social-img' />{' '}
+                    <img src={TwitterLogo} alt="Twitter" className='social-img' />
                   </div>
                 </div>
               </div>
@@ -188,7 +195,7 @@ const ShowAHouse = (props) => {
             <div className='horizontal'>
               <button className='button'>Apply online</button>{' '}
               <Link to='/BookAView'><button className='button-secondary'>Book a viewing</button></Link>
-              <button className='return-search' href="/SearchAHouse">Return to Search <img className='return-img' src={Bed} alt="bed" /></button>
+              <button className='return-search' href="/SearchAHouse">Return to Search <img className='return-img' src={BackArrow} alt="Back" /></button>
             </div>
           </div>
         </div>
